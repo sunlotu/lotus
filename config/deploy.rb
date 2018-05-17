d9053647ad3fcbfead58c0b53b6f87902d81039e
lianhua <itemyaimydwhg@163.com>
2018-05-17 09:43:30 +0800

    capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :branch, :master
 set :deploy_to, "/var/www/#{fetch(:application)}"
 
 # Default value for :format is :airbrussh.
-set :format, :pretty
+# set :format, :pretty
 
 # You can configure the Airbrussh format using :format_options.
 # These are the defaults.
@@ -39,6 +39,7 @@ set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+upload! "#{SECURE_DATA}database.yml", "#{release_path}/config/"
 
 set :nginx_server_name, "sms.demo"
 
